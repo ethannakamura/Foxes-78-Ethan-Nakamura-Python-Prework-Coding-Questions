7e4eac172535c8b0e4a4e429ea06aad00699e1e3
Ethan Nakamura <ethan.nakamura@yahoo.com>
2021-11-17 18:20:02 -0600

    updated questions template

--- Workingfile.py
+++ Workingfile.py
@@ -1,12 +1,3 @@
-#Question 1
-#write a function to print "hello_USENAME!" USENAME is the input of the
-#function. The first line of the code has been defined as below.
-def hello_name():
-    username = input("Enter your username")
-    print("Hello " + username.upper())
-
-hello_name()
-
 #Question 2
 #write a python function, first_odds that prints the odd numbers
 #from 1-100 and returns nothing 
@@ -17,7 +8,7 @@ def first_odds():
 #Please write a Python function, max_num_in_list to return the
 #max number of a given list. The firs tline of the code has 
 #been defined as below.
-def max_num_in_list(a_list):
+
 
 
 #Question 4
@@ -25,7 +16,7 @@ def max_num_in_list(a_list):
 #leap year is divisible by 4, but not divisible by 100, unless
 #it is also divisible by 400. The return should be boolean Type 
 #(true/false)
-def is_leap_year(a_year):
+
 
 
 #Question 5
@@ -33,4 +24,3 @@ def is_leap_year(a_year):
 #numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but
 #[1,2,4,5] are not consecutive numbers. The return should be boolean 
 #Type.
-def is_consecutive(a_list):
